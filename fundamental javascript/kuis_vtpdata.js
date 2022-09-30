5b72914b20c34209e71c8c602687435f524e6f41
Harits <haritsdeveloper@gmail.com>
2022-09-30 20:31:29 +0700

    learning javascript

---
+++ fundamental javascript/kuis_vtpdata.js
@@ -0,0 +1,24 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+ // TODO
+ const firstName = "Harits";
+ const lastName = "budiantoro";
+ let age = 16;
+ let isMarried = false;
+ console.log(firstName, lastName, age, false)
+
+ /**
+  * Jangan hapus kode di bawah ini
+  */
+ 
+ module.exports = {
+   firstName, lastName, age, isMarried,
+ };
+ 
