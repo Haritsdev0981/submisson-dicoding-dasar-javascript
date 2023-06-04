8a56b5f45a2ab467e895a9249329c82437f52960
Harits <haritsdeveloper@gmail.com>
2023-06-04 20:19:02 +0700

    failed to load

--- Testing With Api/main.js
+++ Testing With Api/main.js
@@ -1,7 +1,24 @@
 console.log("Test Guys")
 
 const endpoint = "https://cat-fact.herokuapp.com/facts"
+// const endpoint = "https://regres.in/api/users"
 
-fetch(endpoint)
-    .then(result => result.json())
-    .then(data => console.log(data))
+fetch(endpoint, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        text: "kurakurakura"
+    }),
+})
+    .then((result) => result.json())
+    .then((data) => console.log(data)) 
+
+// async function hitAPI(){
+//     const api = await fetch(endpoint)
+//     const data = await api.json()
+//     console.log(data)
+// }
+
+// hitAPI()
